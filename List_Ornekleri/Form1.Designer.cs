374062051c2e14a8b1e6a250d4bb28d5464007ce
Ayaz Karadag <ayazkaradagg@outlook.com>
2023-12-01 19:02:18 +0300

    Proje dosyası ekle.

---
+++ List_Ornekleri/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace List_Ornekleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listele 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(300, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Listele 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(589, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Listele 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 265);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 29;
+            this.listBox2.Location = new System.Drawing.Point(300, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(256, 265);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 29;
+            this.listBox3.Location = new System.Drawing.Point(589, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(256, 265);
+            this.listBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(890, 373);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ List_Ornekleri/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_Ornekleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> karakterler=new List<string>();
+            karakterler.Add("Ahmet");
+            karakterler.Add("Ayşe");
+            karakterler.Add("Mehmet");
+            karakterler.Add("Halil");
+            karakterler.Add("Yakup");
+            karakterler.Add("Yağız");
+
+            foreach(string k in karakterler)
+            {
+                listBox1.Items.Add(k);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<int> sayilar=new List<int>();
+            sayilar.Add(1);
+            sayilar.Add(10);
+            sayilar.Add(14);               
+            sayilar.Add(19);
+            sayilar.Add(25);
+            sayilar.Add(35);
+            sayilar.Add(45);
+            
+            foreach(int i in sayilar)
+            {
+                if(i % 5==0)
+                {
+                    listBox2.Items.Add(i);
+                }
+
+               /* if (sayilar.Contains(17))
+                {
+                    MessageBox.Show("Bu sayı var");
+                }
+                else
+                {
+                    MessageBox.Show("Bu sayı yok");
+                } */
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<kisiler> kisi = new List<kisiler>();
+            kisi.Add(new kisiler()
+            {
+                ADI = "ALİ",
+                SOYADI = "AKTAŞ",
+                MESLEK = "OGRETMEN"
+            });
+
+            foreach(kisiler k in kisi)
+            {
+                listBox3.Items.Add(k.ADI+" "+k.SOYADI+" "+k.MESLEK);  
+            }
+        }
+    }
+}
